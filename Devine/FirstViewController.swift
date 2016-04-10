4cad4cbeda0cfc101a16273b9efd49e337da16ec
Marc van Eyken <mvaneyken@mvesolutions.com>
2016-04-10 09:53:58 -0400

    Initial Commit

---
+++ Devine/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Devine
+//
+//  Created by Marc van Eyken on 2016-04-10.
+//  Copyright © 2016 MVE Solutiions Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
